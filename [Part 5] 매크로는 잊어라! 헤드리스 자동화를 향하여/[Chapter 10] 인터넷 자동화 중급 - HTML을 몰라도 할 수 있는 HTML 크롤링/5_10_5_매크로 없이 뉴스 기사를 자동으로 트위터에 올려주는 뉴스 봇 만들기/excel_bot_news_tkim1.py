deb65bb6d9b33e084c7b0c5133ad2a789c275a61
Taehwa Kim <kth1149@gmail.com>
2022-07-20 21:39:12 +0900

    commit processing

--- [Part 5] 매크로는 잊어라! 헤드리스 자동화를 향하여/[Chapter 10] 인터넷 자동화 중급 - HTML을 몰라도 할 수 있는 HTML 크롤링/5_10_5_매크로 없이 뉴스 기사를 자동으로 트위터에 올려주는 뉴스 봇 만들기/excel_bot_news_tkim1.py
+++ [Part 5] 매크로는 잊어라! 헤드리스 자동화를 향하여/[Chapter 10] 인터넷 자동화 중급 - HTML을 몰라도 할 수 있는 HTML 크롤링/5_10_5_매크로 없이 뉴스 기사를 자동으로 트위터에 올려주는 뉴스 봇 만들기/excel_bot_news_tkim1.py
@@ -14,3 +14,39 @@ from openpyxl.styles import Font
 from openpyxl.styles import Alignment
 
 # jupyter notebook에서 일단 먼저 실험하는 중 - BeautifulSoup와 Selenium의 연계로
+# BeautifulSoup와 Selenium의 연계로 나만의 코드로 진행
+
+class NewsBot():
+    def __init__(self):
+        self.options = Options()
+        self.options.add_argument('--window-size=1600,900')
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=self.options)
+        self.news_list = []
+        self.news_query = 'https://www.google.com/search?tbm=nws&q='
+
+    def kill(self):
+        self.driver.quit()
+
+    def news_search(self, keyword):
+        self.news_list = []
+        self.driver.get(self.news_query + keyword)
+        self.page = self.driver.page_source
+        self.bs_obj = BS(page, 'html.parser')
+        self.news_obj = self.bs_obj.find_all('div', {'class':'xuvV6b BGxR7d'})
+        for i, news in enumerate(self.news_obj):
+            self.news_company = self.news_obj[i].find('div', {'class':'CEMjEf NUnG9d'}).text
+            self.news_title = self.news_obj[i].find('div', {'class':'mCBkyc y355M ynAwRc MBeuO nDgy9d'}).text.replace('\'', '"')
+            self.news_summary = self.news_obj[i].find('div', {'class':'GI74Re nDgy9d'}).text.replace('\n', '').replace('\'', '"')
+            self.news_period = self.news_obj[i].find('div', {'class':'OSrXXb ZE0LJd'}).text
+            self.news_list.append([self.news_company, self.news_title, self.news_summary, self.news_period])
+
+    def page_crawling(self, number):
+        for i in range(int(number)):
+            self.news_search(self, keyword)
+            self.html_element = self.driver.find_element(By.TAG_NAME, 'html')
+            self.html_element.send_keys(Keys.END)
+            time.sleep(1)
+            self.next_link = self.driver.find_element(By.LINK_TEXT, '다음')
+            self.next_link.click()
+
+# 아직 작성중.. news_search와 page_crawling을 다시 결합해야할 것 같은 느낌..
